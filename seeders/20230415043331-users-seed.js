bd773256c06dbd61d9e346430b19109581fa4e4c
Yuta Sawamura <>
2023-04-16 19:38:40 +0900

    :hammer: Update column name from camel case to snake case

--- seeders/20230415043331-users-seed.js
+++ seeders/20230415043331-users-seed.js
@@ -8,24 +8,24 @@ module.exports = {
         email: 'john@example.com',
         img: 'http://flat-icon-design.com/f/f_object_174/s512_f_object_174_0bg.png',
         password: 'foobar1',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        created_at: new Date(),
+        updated_at: new Date()
       },
       {
         name: 'michael-scott',
         email: 'michael@example.com',
         img: 'http://flat-icon-design.com/f/f_object_161/s512_f_object_161_0bg.png',
         password: 'foobar2',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        created_at: new Date(),
+        updated_at: new Date()
       },
       {
         name: 'catherine',
         email: 'catherine@example.com',
         img: 'http://flat-icon-design.com/f/f_object_108/s512_f_object_108_0bg.png',
         password: 'foobar3',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        created_at: new Date(),
+        updated_at: new Date()
       },
     ], {});
   },
