ee542c430bb25f27553aaeb2aa223268d95693d2
Yuta Sawamura <>
2023-04-16 19:39:22 +0900

    :wrench: Create Model index, user, blog

---
+++ src/models/blog.ts
@@ -0,0 +1,64 @@
+import { DataTypes, Model, Association } from "sequelize";
+import { sequelize } from "./index";
+import { User } from "./user";
+
+export class Blog extends Model {
+  public id!: number;
+  public title!: string;
+  public content!: string;
+  public userId!: number;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+
+  public user!: User;
+  public static associations: {
+    user: Association<Blog, User>;
+  };
+}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'user_id',
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at'
+    },
+  },
+  {
+    tableName: "blogs",
+    sequelize,
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
+
+Blog.belongsTo(User, { foreignKey: "user_id", as: "user" });
+User.hasMany(Blog, { foreignKey: "user_id", as: "blogs" });
