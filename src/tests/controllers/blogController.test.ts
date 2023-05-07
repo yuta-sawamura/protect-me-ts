7f95aba4db052927848819fad604fde4ac093d10
Yuta Sawamura <>
2023-05-07 21:43:49 +0900

    :white_check_mark: Add tests with blogSearch

--- src/tests/controllers/blogController.test.ts
+++ src/tests/controllers/blogController.test.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { blogIndex } from "../../controllers/blogController";
+import { blogIndex, blogSearch } from "../../controllers/blogController";
 import { Blog } from "../../models/blog";
 
 jest.mock("../../models/blog");
@@ -9,24 +9,24 @@ describe("blogController", () => {
     jest.resetAllMocks();
   });
 
+  const mockBlogs = [
+    {
+      id: 1,
+      title: "Sample Blog",
+      content: "This is a sample blog.",
+      userId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      user: {
+        id: 1,
+        name: "John Doe",
+        img: "https://example.com/johndoe.jpg",
+      },
+    },
+  ];
+
   describe("blogIndex", () => {
     it("should render blogs/index with blogs data", async () => {
-      const mockBlogs = [
-        {
-          id: 1,
-          title: "Sample Blog",
-          content: "This is a sample blog.",
-          userId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          user: {
-            id: 1,
-            name: "John Doe",
-            img: "https://example.com/johndoe.jpg",
-          },
-        },
-      ];
-
       (Blog.findAll as jest.Mock).mockResolvedValue(mockBlogs);
 
       const req = {} as Request;
@@ -42,6 +42,39 @@ describe("blogController", () => {
       expect(res.render).toHaveBeenCalledWith("blogs/index", {
         blogs: mockBlogs,
         searchQuery: "",
+      });
+    });
+  });
+
+  describe("blogSearch", () => {
+    it("should render blogs/index with search results", async () => {
+      (Blog.findAll as jest.Mock).mockResolvedValue(mockBlogs);
+
+      const query = "sample";
+      const req = {
+        query: {
+          q: query,
+        },
+      } as unknown as Request;
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+        render: jest.fn(),
+      } as unknown as Response;
+
+      await blogSearch(req, res);
+
+      expect(Blog.findAll).toHaveBeenCalledTimes(1);
+      expect(Blog.findAll).toHaveBeenCalledWith(
+        expect.objectContaining({
+          where: expect.any(Object),
+          include: expect.any(Array),
+        })
+      );
+      expect(res.render).toHaveBeenCalledWith("blogs/index", {
+        blogs: mockBlogs,
+        searchQuery: query,
       });
     });
   });
