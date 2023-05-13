53d9aad17128f702dbf04736c2ec6a1ca1bb704c
Yuta Sawamura <sawamura1009@gmail.com>
2023-05-13 20:25:44 +0900

    :lipstick: Add blog new page (#66)
    Co-authored-by: Yuta Sawamura <>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   blogIndex,
   blogSearch,
+  blogNew,
   blogDetail,
 } from "../controllers/blogController";
 import { scoreIndex } from "../controllers/scoreController";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get("/", blogIndex);
 router.get("/search", blogSearch);
+router.get("/blog/new", blogNew);
 router.get("/blog/:id", blogDetail);
 router.get("/score", scoreIndex);
 
