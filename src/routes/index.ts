22da9a1bd2d4d9ab72c6796dd7969f7f615cfaa9
Yuta Sawamura <>
2023-05-13 15:16:35 +0900

    :hammer: Add blogDetail function and tests in blogController
    - Implement the blogDetail function to handle requests for blog details
    - Add routing for blogDetail in router
    - Update the blog list view to generate dynamic links for each blog post
    - Add tests for the blogDetail function, testing both successful rendering and 404 handling scenarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { blogIndex, blogSearch } from "../controllers/blogController";
+import { blogIndex, blogSearch, blogDetail } from "../controllers/blogController";
 import { scoreIndex } from "../controllers/scoreController";
 
 const router = express.Router();
 
 router.get("/", blogIndex);
 router.get("/search", blogSearch);
+router.get("/blog/:id", blogDetail);
 router.get("/score", scoreIndex);
 
 export default router;
